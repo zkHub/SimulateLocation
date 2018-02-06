c0e0aefa6b5bb161f03391809b130d29bd51aa70
zhangke.bj <zhangke.bj@fang.com>
2018-02-06 16:27:22 +0800

    Initial Commit

---
+++ LocationApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationApp
+//
+//  Created by 张柯 on 2018/2/6.
+//  Copyright © 2018年 张柯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LocationApp
+//
+//  Created by 张柯 on 2018/2/6.
+//  Copyright © 2018年 张柯. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
